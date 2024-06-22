f09bccca1209ef82864ac825e2d761ede8974998
Andre <andriej2301@gmail.com>
2024-06-22 15:18:34 +0200

    feat: Migrate

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    city = models.CharField(max_length=100)
+    district = models.CharField(max_length=100)
+
+    # Interests
+    walk = models.BooleanField(default=False)
+    coffee = models.BooleanField(default=False)
+    tea = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.first_name}, {self.city}"
